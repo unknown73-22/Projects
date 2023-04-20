68007e5ea22fe1da33799b76aeaee99000a7fe2d
Jonas Dallner <dallner.jonas@tutanota.com>
2023-04-20 23:50:37 +0200

    New Code in notion_umlaufwandler

--- Python/Umlautwandler/notion_umlautwandler.py
+++ Python/Umlautwandler/notion_umlautwandler.py
@@ -1,121 +1,39 @@
-import json, requests
 
 import os
 from notion_client import Client
-from datetime import datetime
-import re
 
 class Notion_Umlautwandler:
-
-    file = open('Scret.json')
-
-    data = json.load(file)
     
-    notion = Client(auth=os.environ[data['database_id']])
+    notion = Client(auth=os.environ['secret_m83w1Be6sq8f5BLkj7jumYKPJ1Kaz1JeXcLb1MuVuIw'])
 
-# Initialize a client object with your API key
-    client = Client(auth=data['notion_api_key'])
+    database_id = "1001844ceac24d748760759a3cea0521"
 
-# Specify the ID of your database
-    database_id = data['database_id']
-
-    file.close()
-
-    url = 'https://api.notion.com/v1/pages'
-
-    headers = {
-        'Authorization': f'Bearer {database_id}',
-        'Content-Type': 'application/json',
-        'Notion-version': '2021-08-16'
+    replacements = {
+        "uueee": "ü",
+        "aaeee": "ä",
+        "ooeee": "ö",
+        "ssss": "ß",
+        "Uueee": "Ü",
+        "Aaeee": "Ä",
+        "Ooeee": "Ö"
     }
 
-    data_input = {
-        "parent": { "database_id": f"{database_id}" },
-    "properties": {
-      "title": {
-        "title": [
-          {
-            "text": {
-              "content": "Yurts in Big Sur, California"
-            }
-          }
-        ]
-      }
-    }
-    }
-
-    response = requests.post(url, headers=headers, json= data_input)
- 
-
-# Specify the search query
-    query = "INSERT_SEARCH_QUERY_HERE"    
-
-# Get a reference to the database
-    database = client.databases.retrieve(database_id)
-
-    umlaute = ["aaeee", "Aaeee", "ooeee", "Ooeee", "uueee", "Uueee", "sssss"]
-
-    words_to_replace = []
-    
-    
-    def replace(string):
-        # Replace "aee" with "ä"
-        string = string.replace("aaeee", "ä") # type: ignore
-
-        string = string.replace("Aaeee", "Ä")
-
-# Replace "oee" with "ö"
-        string = string.replace("ooeee", "ö")
-
-        string = string.replace("Ooeee", "Ö")
-
-# Replace "uee" with "ü"
-        string = string.replace("uueee", "ü")
-
-        string = string.replace("Uueee", "Ü")
-
-        string = string.replace("sssss", "ß")
-
-# Create a new page in the database with the transformed string
-   
-
-    results = client.databases.query(
+    results = notion.databases.query(
         **{
             "database_id": database_id,
             "filter": {
                 "property": "Name",
-                "title": {}
+                "title": {
+                    "contains": "Suchbegriff"
+                }
             }
         }
-    )["results"] # type: ignore
+    ).get("results") # type: ignore
 
-# Extract all words from the "Description" property of each item
-    all_words = []
     for result in results:
-        description = result.properties["Description"]["rich_text"][0]["text"]["content"]
-        words = re.findall(r'\w+', description)
-        all_words += words
-
-# Print all words
-    for word in all_words:
-        for umlaute in umlaute:
-            if  umlaute in word:
-                words_to_replace += [word, replace(word)]
-    
-    finds = notion.databases.query(
-        **{
-            "databases_id": database_id,
-            "filter": {
-                "contains": query
-            }
-        }
-    ).get("finds", []) # type: ignore
-
-    for find in finds:
-        name = find.get("properties", {}).get("Name", {}).get("title", [{}])[0].get("plain_text", "")
-
-        new_name = name.replace("TARGET_WORD", "REPLACEMENT_WORD")
-
-        notion.pages.update(find["id"], properties={"Name": {"title": [{"text": {"content": new_name}}]}})
-
-    
+        text = result.properties["Text"].rich_text[0].text.content.lower()
+        for key, value in replacements.items():
+            if key in text:
+                text = text.replace(key, value)
+                result.properties["Text"].rich_text[0].text.content = text
+                notion.pages.update(result["id"], properties=result.properties)
